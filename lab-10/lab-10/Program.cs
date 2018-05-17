dec5ffdff5e038ec6423a449a5b28d8d70dfb65d
Piotr Szołtys <piotrek.szoltys@gmail.com>
2018-05-18 00:10:32 +0200

    finish

--- lab-10/lab-10/Program.cs
+++ lab-10/lab-10/Program.cs
@@ -1,4 +1,4 @@
-﻿// Written by Piotr Szołtys 17.05.2018 'kicks and giggles'
+﻿// Written by Piotr Szołtys 17.05.2018 (maybe 18.05)
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +9,40 @@ namespace lab_10
 {
     class Program
     {
+        private static double BendingForce;
+        private static double TensileForce;
         static void Main(string[] args)
         {
+            CProfile[] profileArr = new CProfile[6];
+            // here comes the part i rly dont like
+            profileArr[0] = new CIBeam(80, 46, 4, 2500, 6);
+            profileArr[1] = new CIBeam(100, 55, 5, 1500, 8.1);
+            profileArr[2] = new CIBeam(120, 64, 5.5, 2000, 10.4);
+            profileArr[3] = new CRoundTube(42.4, 3, 2500, 2.91);
+            profileArr[4] = new CRoundTube(60.3, 3, 1500, 4.24);
+            profileArr[5] = new CRoundTube(101.6, 5, 1000, 11.9);
+
+            foreach(var entry in profileArr)
+            {
+                entry.Show();
+                entry.Show2();
+            }
+            Console.WriteLine("Input bending force value:");
+            BendingForce = GetDouble();
+            Console.WriteLine("Input tensile force value:");
+            TensileForce = GetDouble();
+            var stressArr = new double[6, 2];
+            for(int i = 0; i<6; i++) // FYI: usually we want to keep sizes, string values etc. in some util class so that we can change them rly easy
+            {
+                stressArr[i,0] = CalcBendingStress(profileArr[i].Length, profileArr[i].GetSectionModulus());
+                stressArr[i,1] = CalcTensileStress(profileArr[i].getArea());
+                Console.WriteLine("Bending stress for position: " + i + " is: " + stressArr[i, 0]);
+                Console.WriteLine("Tensile stress for position: " + i + " is: " + stressArr[i, 1]);
+            }
+            Console.ReadKey();
         }
+        private static double GetDouble() => Convert.ToDouble(Console.ReadLine());
+        private static double CalcBendingStress(double length, double secModulus) => (BendingForce*length)/secModulus;
+        private static double CalcTensileStress(double area) => TensileForce / area;
     }
 }
--- lab-10/lab-10/all-in-one.cs
+++ lab-10/lab-10/all-in-one.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// the instruction says to locate all classes in this one file
-
-namespace lab_10
+// whole opl lab 9 code in just 1 file! Programisci go nienawidza, odkryl jeden prostyk trik jak zmiescic wszystko w jednym pliku zobacz jak... (link do strony)
+namespace lab_10_all
 {
+    interface IStrengthParam
+    {
+        double GetInertiaMoment();
+        double GetSectionModulus();
+    }
     class CProfile : IStrengthParam
     {
         private double m_dLength;
@@ -133,7 +137,7 @@ namespace lab_10
         public override double getArea() // usually method name should start from capital letter
         {
             var result = (2 * Thickness * Width) + ((Height - (2 * Thickness)) * Thickness);
-            return Math.Round(result,2); 
+            return Math.Round(result, 2);
         }
         public override double GetSectionModulus()
         {
@@ -192,7 +196,7 @@ namespace lab_10
         }
         public override double getArea() // usually method name should start from capital letter
         {
-            var result = (Math.PI*Math.Pow((Math.Pow(Diameter, 4) - (Diameter - (2*Thickness))),4))/64;
+            var result = (Math.PI * Math.Pow((Math.Pow(Diameter, 4) - (Diameter - (2 * Thickness))), 4)) / 64;
             return Math.Round(result, 2);
         }
         public override double GetSectionModulus()
@@ -202,9 +206,46 @@ namespace lab_10
         }
         public override double GetInertiaMoment()
         {
-            var result = (Math.PI*Math.Pow(((Diameter*Diameter) - (Diameter-(2*Thickness))),2)) / 4;
+            var result = (Math.PI * Math.Pow(((Diameter * Diameter) - (Diameter - (2 * Thickness))), 2)) / 4;
             return Math.Round(result, 2);
         }
     }
+    class Program
+    {
+        private static double BendingForce;
+        private static double TensileForce;
+        static void Main(string[] args)
+        {
+            CProfile[] profileArr = new CProfile[6];
+            // here comes the part i rly dont like
+            profileArr[0] = new CIBeam(80, 46, 4, 2500, 6);
+            profileArr[1] = new CIBeam(100, 55, 5, 1500, 8.1);
+            profileArr[2] = new CIBeam(120, 64, 5.5, 2000, 10.4);
+            profileArr[3] = new CRoundTube(42.4, 3, 2500, 2.91);
+            profileArr[4] = new CRoundTube(60.3, 3, 1500, 4.24);
+            profileArr[5] = new CRoundTube(101.6, 5, 1000, 11.9);
 
+            foreach (var entry in profileArr)
+            {
+                entry.Show();
+                entry.Show2();
+            }
+            Console.WriteLine("Input bending force value:");
+            BendingForce = GetDouble();
+            Console.WriteLine("Input tensile force value:");
+            TensileForce = GetDouble();
+            var stressArr = new double[6, 2];
+            for (int i = 0; i < 6; i++) // FYI: usually we want to keep sizes, string values etc. in some util class so that we can change them rly easy
+            {
+                stressArr[i, 0] = CalcBendingStress(profileArr[i].Length, profileArr[i].GetSectionModulus());
+                stressArr[i, 1] = CalcTensileStress(profileArr[i].getArea());
+                Console.WriteLine("Bending stress for position: " + i + " is: " + stressArr[i, 0]);
+                Console.WriteLine("Tensile stress for position: " + i + " is: " + stressArr[i, 1]);
+            }
+            Console.ReadKey();
+        }
+        private static double GetDouble() => Convert.ToDouble(Console.ReadLine());
+        private static double CalcBendingStress(double length, double secModulus) => (BendingForce * length) / secModulus;
+        private static double CalcTensileStress(double area) => TensileForce / area;
+    }
 }
